758972535dd7d6aee37029de7daaf95e2b5def7d
Sim Ki Hyun <skh417@gmail.com>
2021-12-21 21:51:42 +0900

    add styles

--- my-chart.js
+++ my-chart.js
@@ -1,15 +1,31 @@
-const labels = ["January", "February", "March", "April", "May", "June"];
+const labels = ["1월", "2월", "3월", "4월", "5월"];
+const datasets = [
+  {
+    label: "높음",
+    backgroundColor: "#db3a34",
+    borderColor: "#db3a34",
+    data: [1, 1, 3, 2, 3],
+    maxBarThickness: 150,
+  },
+  {
+    label: "중간",
+    backgroundColor: "#ffc857",
+    borderColor: "#ffc857",
+    data: [12, 21, 33, 42, 13],
+    maxBarThickness: 150,
+  },
+  {
+    label: "낮음",
+    backgroundColor: "#177e89",
+    borderColor: "#177e89",
+    data: [12, 12, 33, 12, 31],
+    maxBarThickness: 150,
+  },
+];
 
 const data = {
-  labels: labels,
-  datasets: [
-    {
-      label: "My First dataset",
-      backgroundColor: "rgb(255, 99, 132)",
-      borderColor: "rgb(255, 99, 132)",
-      data: [0, 10, 5, 2, 20, 30, 45],
-    },
-  ],
+  labels,
+  datasets,
 };
 
 const config = {
